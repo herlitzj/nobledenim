2c8f485612de22628ba037dd0540e2892a2cd282
herlitzj <herlitzj@gmail.com>
2015-04-07 17:56:53 +0000

    finished smarter stock. Need to chase bugs

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,5 +1,5 @@
 class OrderItemsController < ApplicationController
-  before_action :set_order_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_order_item, only: [:show, :edit, :destroy]
   before_action :load_order, only: [:create]
 
   # GET /order_items
@@ -22,7 +22,8 @@ class OrderItemsController < ApplicationController
   # POST /order_items
   # POST /order_items.json
   def create
-    @order_item = @order.order_items.new(quantity: 1, product_id: params[:product_id])
+    @order_item = @order.order_items.find_or_initialize_by(product_id: params[:product_id])
+    @order_item.quantity += 1
 
     respond_to do |format|
       if @order_item.save
@@ -38,13 +39,21 @@ class OrderItemsController < ApplicationController
   # PATCH/PUT /order_items/1
   # PATCH/PUT /order_items/1.json
   def update
-    respond_to do |format|
-      if @order_item.update(order_item_params)
-        format.html { redirect_to @order_item, notice: 'Order item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @order_item.errors, status: :unprocessable_entity }
+    @order_item = OrderItem.find(params[:id])
+    @order_item_quantity = params[:order_item][:quantity]
+    if @order_item_quantity.to_i == 0
+      @order_item.destroy
+      redirect_to orders_url
+      flash = { success: "Item successfully removed.", error: "Item was not removed"}
+    else
+      respond_to do |format|
+        if @order_item.update(order_item_params)
+          format.html { redirect_to orders_url, notice: 'Order item was successfully updated.' }
+          format.json { render :show, status: :ok, location: @order_item }
+        else
+          format.html { render :edit }
+          format.json { render json: @order_item.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
@@ -54,7 +63,7 @@ class OrderItemsController < ApplicationController
   def destroy
     @order_item.destroy
     respond_to do |format|
-      format.html { redirect_to edit_order_path, notice: 'Order item was successfully destroyed.' }
+      format.html { redirect_to orders_path, notice: 'Order item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -71,11 +80,10 @@ class OrderItemsController < ApplicationController
     end
     
     def load_order
-      @order = Order.find_or_initialize_by(params[:order_id])
+      @order = Order.find_or_initialize_by(id: session[:order_id], status: "unsubmitted")
       if @order.new_record?
         @order.save!
         session[:order_id] = @order.id
-        session[:status] = "unsubmitted"
       end
     end
 end
